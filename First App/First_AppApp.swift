42a2aee2260e56f7cee299bed577c5c572833145
Ritika1107 <rgehani6@gatech.edu>
2020-12-10 22:53:45 -0500

    First commit

---
+++ First App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First App
+//
+//  Created by Ritika Gehani on 12/10/20.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
